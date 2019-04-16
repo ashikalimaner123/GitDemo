4bf26ef47c5da6c4f92246081e740ecfe5fed659
ashikali <ashikalimaner786@gmail.com>
2019-04-16 19:53:33 +0530

    Added

--- src/javaPractice/TrialforGit.java
+++ src/javaPractice/TrialforGit.java
@@ -1,9 +1,24 @@
 package javaPractice;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 class TrialforGit {
 
-	public static void main(String[] args) {
-	System.out.println("Ashikali");
+	public static void main(String[] args) throws IOException {
+		
+	FileReader fr=new FileReader(new File("C:\\Users\\Shivam\\Desktop\\NthHighestSalary.txt"));
+	BufferedReader br=new BufferedReader(fr);
+	String a;
+	while((a=br.readLine())!=null)
+	{
+		System.out.println(a);
+	}
+	
+		
 	}
 
+	
 }
